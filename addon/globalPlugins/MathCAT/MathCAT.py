bae9b465cf8c94e85af11b46d0bf8c6b30ff3840
Ryan McCleary <remccleary@gmail.com>
2025-06-01 00:09:41 -0400

    ruff-format

--- addon/globalPlugins/MathCAT/MathCAT.py
+++ addon/globalPlugins/MathCAT/MathCAT.py
@@ -353,7 +353,6 @@ class MathCATInteraction(mathPres.MathInteractionNVDAObject):
 
 	_startsWithMath: re.Pattern = re.compile("\\s*?<math")
 
-
 	@script(
 		# Translators: Message to be announced during Keyboard Help
 		description=_("Copy navigation focus to clipboard"),
